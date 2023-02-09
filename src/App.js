aa3e817acea676e96c9ae01cd2a41488408296dc
Martin Malaza <martinmalaza59@gmail.com>
2023-02-09 22:18:59 +0200

    Form component is responsive.

--- src/App.js
+++ src/App.js
@@ -5,17 +5,27 @@ import Form from './components/Form/Form';
 import Note from './components/Note/Note';
 import './components/Note/Note.css';
 import './components/Modal/Modal.css';
+import { useState } from 'react';
 
 function App() {
+  const [modalState,flipState] = useState({visibility:'hidden'});
+  //let modalState = {visibility:"hidden"};
+  function toggleModal(bin){
+    if (bin) {
+      flipState( {visibility:"visible"});      
+    } else {
+      flipState({visibility:"hidden"});
+    }
+  }
   return (
     <div>
       <Navbar/>
       <Sidebar/>
-      <Form displays={{display:"flex"}}/>
+      <Form displays={{display:"flex"}} toggleModal={toggleModal}/>
       <div className='notes'>
         <Note id={"7848e9a38f3"} title={"Hello world"} text={"This is a test component"}/>
       </div>
-      <div className='modal'/>
+      <div className='modal' style={modalState}/>
     </div>
   );
 }
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -4,72 +4,83 @@ import React,{useState} from 'react';
 var toggleOn = true;
 
 function Form(props){
-    /*var display = {display:"flex"};*/
+    //
     const [display, setDisplay] = useState(props.displays);
+    const [quicks, setQuicks] = useState({display:"block"});
+    const [contexts, setContexts] = useState({display:"none"});
     function formClicked(){
-      //alert("Hello there");
+      props.toggleModal(toggleOn);
       if (toggleOn) {
         setDisplay( {display:"block"});
-        toggleOn=false;
+        setQuicks({display:"none"});
+        setContexts({display:"block"});
+        
       }else{
-        console.log("Closed");
         setDisplay({display:"flex"});
+        setQuicks({display:"block"});
+        setContexts({display:"none"});
         toggleOn = true;
       }
+    }
+    
+    function closeBtn(event){
+      event.preventDefault();
+      console.log("Button instead",event.target);
+      toggleOn=false;
     }
     return (
       <div className="form-container inactive-form" onClick={formClicked}>
         <form style={display}>
-            <input type="text" className="note-title context" placeholder="Title" />
+            <input type="text" className="note-title" placeholder="Title" style={contexts}/>
             <input type="text" className="note-title note-text" placeholder="Take a note..." />
           <div className="form-actions">
             <div className="icons">
-            <div className="tooltip quick">
+            <div className="tooltip" style={quicks}>
               <span className="material-icons-outlined hover">check_box</span>
               <span className="tooltip-text">New List</span>
             </div>
-            <div className="tooltip quick">
+            <div className="tooltip" style={quicks}>
               <span className="material-icons-outlined hover">brush</span>
               <span className="tooltip-text">New Drawing</span>
             </div>
-            <div className="tooltip quick">
+            <div className="tooltip" style={quicks}>
               <span className="material-icons-outlined hover">image</span>
               <span className="tooltip-text">New Image</span>
             </div>
-            <div className="tooltip context">
+            <div className="tooltip" style={contexts}>
               <span className="material-icons-outlined hover small-icon">add_alert</span>
               <span className="tooltip-text">Remind me</span>
             </div>
-            <div className="tooltip context">
+            <div className="tooltip" style={contexts}>
               <span className="material-icons-outlined hover small-icon">person_add</span>
               <span className="tooltip-text">Collaborator</span>
             </div>
-            <div className="tooltip context">
+            <div className="tooltip" style={contexts}>
               <span className="material-icons-outlined hover small-icon">palette</span>
               <span className="tooltip-text">Change Color</span>
             </div>
-            <div className="tooltip context">
+            <div className="tooltip" style={contexts}>
               <span className="material-icons-outlined hover small-icon">image</span>
               <span className="tooltip-text">Add Image</span>
             </div>
-            <div className="tooltip context">
+            <div className="tooltip" style={contexts}>
               <span className="material-icons-outlined hover small-icon">archive</span>
               <span className="tooltip-text">Archive</span>
             </div>
-            <div className="tooltip context">
+            <div className="tooltip" style={contexts}>
               <span className="material-icons-outlined hover small-icon">more_vert</span>
               <span className="tooltip-text">More</span>
             </div>
-            <div className="tooltip context">
+            <div className="tooltip" style={contexts}>
               <span className="material-icons-outlined hover small-icon">undo</span>
               <span className="tooltip-text">Undo</span>
             </div>
-            <div className="tooltip context">
+            <div className="tooltip" style={contexts}>
               <span className="material-icons-outlined hover small-icon">redo</span>
               <span className="tooltip-text">Redo</span>
             </div>
           </div>
-            <button type="button" className="close-btn context">Close</button>
+            <button type="button" className="close-btn" onClick={closeBtn} style={contexts}>Close</button>
             
           </div>
         </form>
